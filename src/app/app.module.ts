57fcdeb7884a6ba285800898e1bbb29f5b8fa3f2
Dmitriy Mishchenko <ripatrip@gmail.com>
2023-09-03 19:12:47 +0300

    add simple pwa and caching

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { HttpClientModule } from '@angular/common/http'
+import { ServiceWorkerModule } from '@angular/service-worker'
 import { AppComponent } from './app.component'
 import { PLAYBACK_REPOSITORY_TOKEN } from './base/tokens'
 import { PlaybackPlayerModule } from './playback-player/playback-player.module'
 import { PlaybackRepositoryService } from './repositories/playback-repository.service'
 @NgModule({
-  imports: [BrowserModule, HttpClientModule, PlaybackPlayerModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    PlaybackPlayerModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: true,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
   declarations: [AppComponent],
   providers: [{ provide: PLAYBACK_REPOSITORY_TOKEN, useClass: PlaybackRepositoryService }],
   bootstrap: [AppComponent]
